0cedd6a54d8c55c4ec91a1f712031d191b22de6d
846c/crxcket <84820661+crcket@users.noreply.github.com>
2025-10-30 13:17:29 -0400

    Create AutoHop_Centralized.lua

---
+++ AutoHop_Centralized.lua
@@ -0,0 +1,10 @@
+local GithubChecker = loadstring(game:HttpGet("https://raw.githubusercontent.com/crcket/ROBLOX/refs/heads/main/GithubSHAChecker.lua"))()
+local CheckFunc = GithubChecker.CheckGithubSHA
+local ExecuteUrl = "https://raw.githubusercontent.com/crcket/YBA/refs/heads/main/AutoHop.lua"
+if getgenv().Settings.DevMode then
+    ExecuteUrl = "https://raw.githubusercontent.com/crcket/YBA/refs/heads/main/AutoHopInDev.lua"
+else
+    ExecuteUrl = "https://raw.githubusercontent.com/crcket/YBA/refs/heads/main/AutoHop.lua" -- redundancy
+end
+getgenv().AutoHopVersion = CheckFunc(ExecuteUrl)
+loadstring(request({Url = Url}).Body)()
